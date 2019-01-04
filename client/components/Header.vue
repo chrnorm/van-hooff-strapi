792ed5f9bfe9038ec658c87a90716b24113af890
Dries Bos <info@driesbos.com>
2019-01-04 18:39:30 +0100

    header

---
+++ client/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="header">
+    <ul>
+      <router-link tag="li" to="/" exact>Deliveroo clone</router-link>
+    </ul>
+  </header>
+</template>
+
+<style lang="sass" scoped>
+header
+  background: grey
+</style>
