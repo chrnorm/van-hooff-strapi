c85597a5aea24668e7e1e9cbe37b8a7c747ab344
Dries Bos <info@driesbos.com>
2019-01-06 22:10:37 +0100

    cleanup

--- client/components/SliderItem.vue
+++ client/components/SliderItem.vue
@@ -43,8 +43,42 @@ export default {
 </script>
 
 <style lang="sass">
-  .slider-enter-active, .slider-leave-active
-    transition: all .2s ease
-  .slider-enter, .slider-leave-to
-    opacity: 0
+.slider-Container
+  position: relative
+  z-index: 995
+  height: 100%
+.image-Slider
+  position: absolute
+  top: 0
+  left: 0
+  height: 100%
+  img
+    width: 100%
+    height: 100%
+    object-fit: contain
+    object-position: center center
+.image-Slider_Nav
+  position: absolute
+  width: 33%
+  top: 0
+  bottom: 0
+  cursor: pointer
+.image-Slider_Prev
+  left: 0
+  cursor: url(~/assets/images/prev-icon.svg), w-resize
+.image-Slider_Close
+  left: 50%
+  transform: translateX(-50%)
+  cursor: url("~/assets/images/close-icon.png") 0 0, pointer
+  cursor: url("~/assets/images/close-icon.svg") 0 0, pointer
+  cursor: -webkit-image-set(url("~/assets/images/close-icon.png") 1x, url("~/assets/images/close-icon.png") 2x) 0 0, pointer
+  cursor: url(~/assets/images/close-icon.svg), zoom-out
+.image-Slider_Next
+  right: 0
+  cursor: url(~/assets/images/next-icon.svg), e-resize
+
+.slider-enter-active, .slider-leave-active
+  transition: all .2s ease
+.slider-enter, .slider-leave-to
+  opacity: 0
 </style>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -20,4 +20,3 @@ export default {
 #__layout
   padding: $spacing
 </style>
-
