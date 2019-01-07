56323f3a2a3b61adc1dca57e6e0e3c663e2f4900
Dries Bos <info@driesbos.com>
2019-01-07 19:16:19 +0100

    project-single pages

--- client/pages/projects/_id/index.vue
+++ client/pages/projects/_id/index.vue
@@ -1,21 +1,56 @@
 <template>
-  <!-- single project -->
-  <div class="page-Container">
-    <ul :key="project.id" v-for="project in projects">
-      <li>
-        <p>{{ project.title }}</p>
-        <p>{{ project.text }}</p>
-      </li>
-    </ul>
+  <div>
+    <div class="productItem">
+      <ul :key="project.id" v-for="project in projects">
+        <li>
+          <div class="product-Image" @click="showModal = true">
+            <img :src="project.cover.url">
+          </div>
+          <div class="product-TitleDate">
+            <div>
+              <h1>{{ project.title }}</h1>
+            </div>
+            <div>
+              <p>{{ project.category }}</p>
+            </div>
+          </div>
+          <p class="productItem-Text">{{ project.text }}</p>
+          <div class="product-Footer">
+            <router-link tag="div" to="/Product" class="product-Footer_Prev">
+              <img class="arrow" src="@/assets/images/arrow.png">
+              <p>Previous Project</p>
+            </router-link>
+            <router-link tag="div" to="/Product" class="product-Footer_Next">
+              <p>Next</p>
+              <img class="arrow" src="@/assets/images/arrow.png">
+            </router-link>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <modalItem v-if="showModal" @close="showModal = false">
+      <sliderItem @close="showModal = false"></sliderItem>
+    </modalItem>
   </div>
 </template>
 
 <script>
 import Strapi from 'strapi-sdk-javascript/build/main'
+import SliderItem from '@/components/SliderItem.vue'
+import ModalItem from '@/components/ModalItem.vue'
 const apiUrl = process.env.API_URL || 'http://localhost:1337'
 const strapi = new Strapi(apiUrl)
 
 export default {
+  components: {
+    SliderItem,
+    ModalItem
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  },
   computed: {
     id() {
       return this.$route.params.id
@@ -32,6 +67,7 @@ export default {
             project(id: "${params.id}") {
               _id
               title
+              category
               text
               cover {
                 url
@@ -44,12 +80,11 @@ export default {
           `
       }
     })
-    response.data.project.forEach(project => {
-      project.cover.url = `${apiUrl}${project.cover.url}`
-      store.commit('projects/add', {
-        id: project.id || project._id,
-        ...project
-      })
+    let project = response.data.project
+    project.cover.url = `${apiUrl}${project.cover.url}`
+    store.commit('projects/add', {
+      id: project.id || project._id,
+      ...project
     })
   }
 }
