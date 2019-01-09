378ce97207adc937abf933bdaed4c68a1a70de4e
Chris Norman <chris@humm.tech>
2019-01-09 19:43:30 +0800

    change api url to heroku

--- client/pages/projects/index.vue
+++ client/pages/projects/index.vue
@@ -24,7 +24,7 @@
 
 <script>
 import Strapi from 'strapi-sdk-javascript/build/main'
-const apiUrl = process.env.API_URL || 'http://localhost:1337'
+const apiUrl = process.env.API_URL || 'https://van-hooff.herokuapp.com'
 const strapi = new Strapi(apiUrl)
 
 export default {
